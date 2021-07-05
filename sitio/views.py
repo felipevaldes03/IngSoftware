cb7289fb91452ed415652176c16f52abbe9a1ade
saul zarate <saul.zarate@davinci.edu.ar>
2021-07-05 20:20:53 -0300

    vista de productos por categoria hecha

--- sitio/views.py
+++ sitio/views.py
@@ -30,7 +30,16 @@ def producto_eliminar(request, producto_id):
     return HttpResponse('Eliminar Producto con id: ' + str(producto_id))
 
 def productos_por_categoria(request, categoria_id):
-    return HttpResponse('Categoria con id: ' + str(categoria_id))
+    categoria = Categoria.objects.get(id=categoria_id)
+    productos = categoria.productos.all()
+    return render(request, 'sitio/producto/resultado_busqueda.html',
+    {
+        'categorias' : Categoria.objects.all(),
+        'productos' : productos,
+        'categoria' : categoria.descripcion,
+        'titulo_seccion' : 'Productos de la categoria',
+        'sin_productos' : 'No hay producto de la categoria ' + categoria.descripcion
+    })
 
 """ 
     PAGINAS
