7b5082a1369c6ecdb3e074a2a154631426958ec6
saul zarate <saul.zarate@davinci.edu.ar>
2021-07-05 23:52:45 -0300

    vista de detalle de producto hecho

--- sitio/urls.py
+++ sitio/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     #path('producto/agregar', views.producto_agregar, name="producto_agregar" ),
     path('producto/<int:producto_id>', views.producto_show, name="producto_show" ),
     path('producto/<int:producto_id>/editar', views.producto_editar, name="producto_editar" ),
-    path('producto/<int:producto_id>/eliminar', views.producto_eliminar, name="producto_eliminar" ),
+    path('producto/<int:producto_id>/eliminar', views.producto_delete, name="producto_eliminar" ),
+    path('producto/buscador', views.producto_search, name="producto_search"),
     # Productos por categorias
     path('categoria/<int:categoria_id>/productos', views.productos_por_categoria, name="productos_por_categoria"),
 
--- sitio/views.py
+++ sitio/views.py
@@ -21,18 +21,37 @@ def producto_agregar(request):
     pass
 
 def producto_show(request, producto_id):
-    return HttpResponse('Ver Producto con id: ' + str(producto_id))
+    producto = Producto.objects.get(id=producto_id)
+
+    return render(request, 'sitio/producto/show.html',{
+        'categorias' : Categoria.objects.all(),
+        'producto' : producto
+    })
 
 def producto_editar(request, producto_id):
     return HttpResponse('Editar Producto con id: ' + str(producto_id))
 
-def producto_eliminar(request, producto_id):
+def producto_delete(request, producto_id):
     return HttpResponse('Eliminar Producto con id: ' + str(producto_id))
+
+def producto_search(request):
+    texto_de_busqueda = request.GET["texto"]
+    productosPorTitulo = Producto.objects.filter(titulo__icontains = texto_de_busqueda).all()
+    productosPorDescripcion = Producto.objects.filter(descripcion__icontains = texto_de_busqueda).all()
+    productos = productosPorTitulo | productosPorDescripcion
+    return render(request, 'sitio/producto/search.html',
+    {
+        'categorias' : Categoria.objects.all(),
+        'productos' : productos,
+        'texto_buscado' : texto_de_busqueda,
+        'titulo_seccion' : 'Productos que contienen',
+        'sin_productos' : 'No hay producto de la categoria ' + texto_de_busqueda
+    })
 
 def productos_por_categoria(request, categoria_id):
     categoria = Categoria.objects.get(id=categoria_id)
     productos = categoria.productos.all()
-    return render(request, 'sitio/producto/resultado_busqueda.html',
+    return render(request, 'sitio/producto/search.html',
     {
         'categorias' : Categoria.objects.all(),
         'productos' : productos,
@@ -45,4 +64,6 @@ def productos_por_categoria(request, categoria_id):
     PAGINAS
 """
 def acerca_de(request):
-    return render(request, 'sitio/paginas/acerca_de.html')
+    return render(request, 'sitio/paginas/acerca_de.html',{
+        'categorias' : Categoria.objects.all(),
+    })
